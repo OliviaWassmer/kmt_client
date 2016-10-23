390db6e8fdfbe4ddc1f91cc71aa5d13a328bb9a1
OliviaWassmer <olivia.wassmer@stud.hslu.ch>
2016-10-23 12:58:45 +0200

    DBHandler erstellt
    muss noch implemnetiert werden

--- KMT_App/app/src/main/java/ch/hslu/tem31/kmt/kmt_app/DatabaseHandler.java
+++ KMT_App/app/src/main/java/ch/hslu/tem31/kmt/kmt_app/DatabaseHandler.java
@@ -2,6 +2,8 @@ package ch.hslu.tem31.kmt.kmt_app;
 
 /**
  * Created by olivi on 21.10.2016.
+ * Klasse in der ich die Kommunikation mit der NoSQL DB auf dem Amazon Cloud Server herstelle.
+ * Bietet Methoden welche die Daten an die GUI Implementation weitergeben können.
  */
 
 public class DatabaseHandler {
