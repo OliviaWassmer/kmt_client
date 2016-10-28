41a942ca62a2b15e2e5cb4d1cd5a97faefe60113
bananaah <bettinawilli@Bettinas-MacBook-Air.local>
2016-10-28 10:37:02 +0200

    test comment

---
+++ KMT_App/app/src/main/gen/ch/hslu/tem31/kmt/kmt_app/Manifest.java
@@ -0,0 +1,7 @@
+/*___Generated_by_IDEA___*/
+
+package ch.hslu.tem31.kmt.kmt_app;
+
+/* This stub is only used by the IDE. It is NOT the Manifest class actually packed into the APK */
+public final class Manifest {
+}
