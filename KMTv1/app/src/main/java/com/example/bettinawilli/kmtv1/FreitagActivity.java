0f1401e33521821c6036f8320c9c67ba540ae17f
OliviaWassmer <olivia.wassmer@stud.hslu.ch>
2016-11-12 17:30:17 +0100

    Dynamo Probleme gefixt

--- KMTv1/app/src/main/java/com/example/bettinawilli/kmtv1/FreitagActivity.java
+++ KMTv1/app/src/main/java/com/example/bettinawilli/kmtv1/FreitagActivity.java
@@ -2,6 +2,10 @@ package com.example.bettinawilli.kmtv1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.example.bettinawilli.kmtv1.mobile.AWSMobileClient;
+import com.example.bettinawilli.kmtv1.models.nosql.EventlistDO;
 
 public class FreitagActivity extends AppCompatActivity {
 
@@ -9,5 +13,20 @@ public class FreitagActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_freitag);
+
+        //owa_das sollte in einer initialize application klasse generalistisch gemacht werden und nicht für jede activit
+        AWSMobileClient.initializeMobileClientIfNecessary(getApplicationContext());
+        AWSMobileClient client = AWSMobileClient.defaultMobileClient();
+
+        Log.i("com.example", ""+client.getDynamoDBClient().listTables());
+
+        /*owa_Error weil DAten nicht in Mainthread von Activity geholt werden dürfen,
+         müssen einen zweiten Thread dafür haben - Stichwort IntentService der das handelt
+
+          */
     }
+
+
+
+
 }
