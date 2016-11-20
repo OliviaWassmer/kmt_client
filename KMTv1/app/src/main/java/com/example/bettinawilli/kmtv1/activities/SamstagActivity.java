d11fe4418736967e5ad944245bfcf944d908199e
OliviaWassmer <olivia.wassmer@stud.hslu.ch>
2016-11-20 17:28:32 +0100

    adapt to samstag
    und natürli crasht alles.... thread problem wider.. ich schaffe dra

--- KMTv1/app/src/main/java/com/example/bettinawilli/kmtv1/activities/SamstagActivity.java
+++ KMTv1/app/src/main/java/com/example/bettinawilli/kmtv1/activities/SamstagActivity.java
@@ -2,6 +2,7 @@ package com.example.bettinawilli.kmtv1.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -9,6 +10,7 @@ import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBScanExpr
 import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.PaginatedList;
 import com.example.bettinawilli.kmtv1.R;
 import com.example.bettinawilli.kmtv1.mobile.AWSMobileClient;
+import com.example.bettinawilli.kmtv1.models.nosql.EventsFreitagDO;
 import com.example.bettinawilli.kmtv1.models.nosql.EventsSamstagDO;
 
 import java.util.ArrayList;
@@ -17,7 +19,7 @@ import java.util.List;
 public class SamstagActivity extends AppCompatActivity {
 
     ListView listViewSamstag;
-    List<String> eventListSamstag = new ArrayList<String>();
+    //List<String> eventListSamstag = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,10 +27,16 @@ public class SamstagActivity extends AppCompatActivity {
         setContentView(R.layout.activity_samstag);
         listViewSamstag = (ListView) findViewById(R.id.listViewSamstag);
 
-        //owa_initialisiert
+        //initialisiert
         AWSMobileClient.initializeMobileClientIfNecessary(getApplicationContext());
 
-        Runnable runnable = new Runnable() {
+        //ArrayAdapter für ListView erstellen
+        final ArrayAdapter<String> adapterSamstag = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, new ArrayList<String>());
+
+        //Array in ListView abfüllen
+        listViewSamstag.setAdapter(adapterSamstag);
+
+        final Runnable runnable = new Runnable() {
             @Override
             public void run() {
                 AWSMobileClient client = AWSMobileClient.defaultMobileClient();
@@ -37,30 +45,36 @@ public class SamstagActivity extends AppCompatActivity {
                 PaginatedList<EventsSamstagDO> eventList = client.getDynamoDBMapper().scan(EventsSamstagDO.class, scanExpression);
 
                 //Alle Events aus der DB in eine Liste abfüllen.
+                final ArrayList<String> samstagList = new ArrayList<String>();
+                //Alle Events aus der DB in eine Liste abfüllen.
                 if(eventList.size() > 0) {
                     for(int i=0; i<eventList.size();i++){
-                        setEventDataOnList(eventList.get(i));
+                        setEventDataOnList(samstagList, eventList.get(i));
                     }
                 } else {
-                    eventListSamstag.add("Keine Events gefunden");
+                    samstagList.add("Keine Events gefunden");
                 }
+
+                SamstagActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Log.i("bla", "Updating data...");
+                        adapterSamstag.clear();
+                        adapterSamstag.addAll(samstagList);
+                        adapterSamstag.notifyDataSetChanged();
+                    }
+                });
             }
         };
-
-        //ArrayAdapter für ListView erstellen
-        ArrayAdapter<String> adapterFreitag = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, eventListSamstag);
-
-        //Array in ListView abfüllen
-        listViewSamstag.setAdapter(adapterFreitag);
 
         Thread thread = new Thread(runnable);
         thread.start();
 
     }
 
-    private void setEventDataOnList(EventsSamstagDO event){
+    private void setEventDataOnList(ArrayList<String> samstagList, EventsSamstagDO event){
 
-        eventListSamstag.add(event.getZeit() + " Uhr: "+ "\t" + "\t" + event.getAct());
+        samstagList.add(event.getZeit() + " Uhr: "+ "\t" + "\t" + event.getAct());
     }
 
 
